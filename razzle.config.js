6497a109fe04331f765178b548c345840465d2c6
MarkLyck <mark.lyck@gmail.com>
2018-04-07 13:59:57 +0200

    offline-plugin caches all

--- razzle.config.js
+++ razzle.config.js
@@ -8,13 +8,7 @@ module.exports = {
             config.plugins.push(new OfflinePlugin({
                 relativePaths: false,
                 publicPath: config.output.publicPath,
-                caches: {
-                    main: [':rest:'],
-                    // All chunks marked as `additional`, loaded after main section
-                    // and do not prevent SW to install. Change to `optional` if
-                    // do not want them to be preloaded at all (cached only when first loaded)
-                    additional: ['*.chunk.js'],
-                },
+                caches: 'all',
                 safeToUseOptionalCaches: true,
                 AppCache: false,
                 ServiceWorker: {
